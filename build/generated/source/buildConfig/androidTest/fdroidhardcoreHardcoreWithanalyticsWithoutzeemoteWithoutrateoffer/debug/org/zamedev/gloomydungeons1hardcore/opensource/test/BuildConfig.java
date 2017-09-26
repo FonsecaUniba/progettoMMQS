811cfda338be2a28c3b0ee9e39433b00ce5e1100
n.fonseca <n.fonseca@1d516f38-3585-4a29-96bf-8b6d8d027a5b>
2017-09-26 13:25:02 +0000

    Caricamento codice iniziale per fresh start

---
+++ build/generated/source/buildConfig/androidTest/fdroidhardcoreHardcoreWithanalyticsWithoutzeemoteWithoutrateoffer/debug/org/zamedev/gloomydungeons1hardcore/opensource/test/BuildConfig.java
@@ -0,0 +1,22 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.zamedev.gloomydungeons1hardcore.opensource.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.zamedev.gloomydungeons1hardcore.opensource.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "fdroidhardcoreHardcoreWithanalyticsWithoutzeemoteWithoutrateoffer";
+  public static final int VERSION_CODE = 1730030000;
+  public static final String VERSION_NAME = "2017.30.03";
+  public static final String FLAVOR_package = "fdroidhardcore";
+  public static final String FLAVOR_difficulty = "hardcore";
+  public static final String FLAVOR_analytics = "withanalytics";
+  public static final String FLAVOR_zeemote = "withoutzeemote";
+  public static final String FLAVOR_rateoffer = "withoutrateoffer";
+  // Fields from product flavor: withanalytics
+  public static final boolean WITH_ANALYTICS = true;
+  // Fields from product flavor: withoutzeemote
+  public static final boolean WITH_ZEEMOTE = false;
+}
